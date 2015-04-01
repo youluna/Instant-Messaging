2fdfc2ec9713db575c407e345e5e4783bedfd5bc
zhaoguoyan <b121220147@114.212.191.43>
2015-04-01 15:49:59 -0700

    basic already

--- client.c
+++ client.c
@@ -5,29 +5,320 @@
 #include<netinet/in.h>
 #include<string.h>
 #include<arpa/inet.h>
+#include<pthread.h>
+
 #define MAXLINE 4096
 #define SERV_PORT 3000
-
-int main(int argc,char **argv)
+int sockfd;
+struct SENDTOCLI
 {
-	int sockfd;
-	struct sockaddr_in servaddr;
-	char sendline[MAXLINE],recvline[MAXLINE];
+	char ctrl1;
+	char ctrl2;
+	char from[10];
+	char dst[10];
+	char data[2048];
 
-	if(argc !=2){
-		perror("Usage:TCPClient<IP address of the server>");
-		exit(1);
+};
+struct CLIENT_SEND
+{
+	char ctrl1;
+	char ctrl2;
+	char local_name[10];//your name
+	char local_key[10];
+	char dst_name[10];//whom to send to
+	char data[2048];
+};
+struct SENDTOCLI recvl;
+struct CLIENT_SEND sendl;
+char sendline[MAXLINE];
+char recvline[MAXLINE];
+void *mainThread(void *j);
+void *recvThread(void *j);
+void login();
+void unlogin();
+void regist();
+void recvmsg();
+void sendmsg();
+
+
+/*-----------------------mainThread()  recv everything but words------------------------*/
+void *mainThread(void *j){
+
+		recvmsg();//system dignals (except words send to you)
+
+}
+
+/*-----------------------recvThread()  only recv the words someone send to you------------------------*/
+void *recvThread(void *j){
+	recv(sockfd,recvline,MAXLINE,0);
+	recvl.ctrl1=recvline[0];
+	recvl.ctrl2=recvline[1];
+	//string to stuct
+	for (int i = 0; i < 10; ++i)
+	{
+		recvl.from[i]=recvline[i+2];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		recvl.dst[i]=recvline[i+12];
+	}
+	for (int i = 0;recvline[i+22]!='\0'; ++i)
+	{
+		recvl.data[i]=recvline[i+22];
 	}
 
+	if (recvl.ctrl1=='4'&&recvl.ctrl2=='3')//some one send you words
+	{
+		printf("%s\n",recvl.data );
+		
+	}
+}
+
+void unlogin(){
+	sendl.ctrl1=='3';
+	sendl.ctrl2=='1';
+	sendline[0]=sendl.ctrl1;
+	sendline[1]=sendl.ctrl2;
+	send(sockfd,sendline,MAXLINE,0);
+}
+
+void login(){
+	char name[10]={'0'};
+	char key[10]={'0'};
+	sendl.ctrl1='1';
+	sendl.ctrl2='1';
+	printf("%s\n","Please input your name and password(login):" );
+	scanf("%s",name);
+	scanf("%s",key);
+	strcpy(sendl.local_name,name);
+	strcpy(sendl.local_key,key);
+
+	sendline[0]=sendl.ctrl1;
+	sendline[1]=sendl.ctrl2;
+	for(int i = 0; i < 10; i++)
+	{
+		sendline[2+i]=sendl.local_name[i];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		sendline[12+i]=sendl.local_key[i];
+	}
+	send(sockfd,sendline,MAXLINE,0);
+
+}
+void regist(){
+	char name[10]={'0'};
+	char key[10]={'0'};
+	sendl.ctrl1='1';
+	sendl.ctrl2='2';
+	printf("%s\n","Please input your name and password(regist):" );
+	scanf("%s",name);
+	scanf("%s",key);
+	strcpy(sendl.local_name,name);
+	strcpy(sendl.local_key,key);
+
+	sendline[0]=sendl.ctrl1;
+	sendline[1]=sendl.ctrl2;
+	for(int i = 0; i < 10; i++)
+	{
+		sendline[2+i]=sendl.local_name[i];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		sendline[12+i]=sendl.local_key[i];
+	}
+	send(sockfd,sendline,MAXLINE,0);
+}
+void sendmsg(){
+
+	sendl.ctrl1='2';
+	char choose[10];
+	char to_name[10];
+	char data[2048];
+	printf("%s\n","PLease input the numbers you want to send:" );
+	printf("%s\n","1:all" );
+	printf("%s\n","others:certain one" );
+	scanf("%s",choose);
+	if (strcmp(choose,"1"))
+	{
+		sendl.ctrl2='2';
+	}
+	else {
+		sendl.ctrl1='1';
+		printf("%s\n","Please input his name" );
+		scanf("%s",to_name);
+		strcpy(sendl.dst_name,to_name);
+	}
+	
+	printf("%s\n","Please input your words:" );
+	scanf("%s",data);
+	strcpy(sendl.data,data);
+
+	sendline[0]=sendl.ctrl1;
+	sendline[1]=sendl.ctrl2;
+	for(int i = 0; i < 10; i++)
+	{
+		sendline[2+i]=sendl.local_name[i];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		sendline[22+i]=sendl.dst_name[i];
+	}
+	for (int i = 0; sendl.data[i]!='\0'; ++i)
+	{
+		sendline[32+i]=sendl.data[i];
+	}
+
+	send(sockfd,sendline,MAXLINE,0);
+
+}
+void recvmsg(){
+	recv(sockfd,recvline,MAXLINE,0);
+	recvl.ctrl1=recvline[0];
+	recvl.ctrl2=recvline[1];
+	//string to stuct
+	for (int i = 0; i < 10; ++i)
+	{
+		recvl.from[i]=recvline[i+2];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		recvl.dst[i]=recvline[i+12];
+	}
+	for (int i = 0;recvline[i+22]!='\0'; ++i)
+	{
+		recvl.data[i]=recvline[i+22];
+	}
+
+
+	char choose[10];
+	if ((recvl.ctrl1=='0'&&recvl.ctrl2=='0')||(recvl.ctrl1=='3'&&recvl.ctrl2=='2')||(recvl.ctrl1=='3'&&recvl.ctrl2=='4'))
+	{
+		
+		printf("%s\n","Please input numbers:" );
+		printf("%s\n","1:login" );
+		printf("%s\n","2:regist" );
+		printf("%s\n","3:quit" );
+		scanf("%s",choose);
+		if (strcmp(choose,"1"))
+		{
+			login();
+		}else if (strcmp(choose,"2"))
+		{
+			regist();
+		}else if (strcmp(choose,"3"))
+		{
+			unlogin();
+			exit(0);
+		}
+		else printf("%s\n","error!" );
+	}
+	else {
+		
+
+	if (recvl.ctrl1=='3')
+	{
+		if (recvl.ctrl2=='1')
+		{
+			printf("%s\n","Login succeed!" );
+			sendl.ctrl1=='2';
+		}
+		else if (recvl.ctrl2=='2')
+		{
+			printf("%s\n","Login failed!" );
+		}
+		else if (recvl.ctrl2=='3')
+		{
+			printf("%s\n","unlogin succeed!" );
+		}
+		else if (recvl.ctrl2=='4')
+		{
+			printf("%s\n", "unlogin failed!");
+		}
+	}
+	else if (recvl.ctrl1=='4')
+	{
+		if (recvl.ctrl2=='1')
+		{
+			printf("%s\n","Message send succeed!" );
+		}
+		else if (recvl.ctrl2=='2')
+		{
+			printf("%s\n","Message send failed" );
+		}
+	}
+	else if (recvl.ctrl1=='5')
+	{
+		if (recvl.ctrl2=='1')
+		{
+			printf("%s\n","unlogin succeed!" );
+			exit(0);
+		}
+		else if (recvl.ctrl2=='2')
+		{
+			printf("%s\n","unlogin failed" );
+		}
+	}
+
+	if (!(recvl.ctrl1=='3'&&recvl.ctrl2=='2')||!(recvl.ctrl1=='3'&&recvl.ctrl2=='4'))
+	{
+	
+	printf("%s\n","Please input numbers:" );
+		printf("%s\n","1:send message" );
+		printf("%s\n","2:log out" );
+		printf("%s\n","3:quit" );
+		scanf("%s",choose);
+		if (strcmp(choose,"1"))
+		{
+			sendmsg();
+		}
+		else if (strcmp(choose,"2"))
+		{
+			unlogin();
+		}
+		else if (strcmp(choose,"3"))
+		{
+			unlogin();
+			exit(0);
+		}
+	}
+
+}
+}
+int main()
+{
+	printf("ddd11111111111ddd");
+	
+	struct sockaddr_in servaddr;
+
+	printf("dddddddddddddddddd");
 	sockfd=socket(AF_INET,SOCK_STREAM,0);
 
 	memset(&servaddr,0,sizeof(servaddr));
 	servaddr.sin_family=AF_INET;
-	servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+	servaddr.sin_addr.s_addr=inet_addr("192.168.113.128");
 	servaddr.sin_port=htons(SERV_PORT);
 
-	connect(sockfd,(struct sockaddr *)&servaddr ,sizeof(servaddr));
+	printf("dddddd");
 
+	connect(sockfd,(struct sockaddr *)&servaddr ,sizeof(servaddr));
+//didn't login
+	recvl.ctrl1='0';
+	recvl.ctrl2='0';
+	pthread_t main_thread;
+	pthread_t recv_thread;
+	printf("dddaaaaaaadd");
+	send(sockfd,sendline,MAXLINE,0);
+	sendline[0]='8';
+
+	if(pthread_create(&main_thread,NULL,mainThread,NULL)!=0)
+		printf("%s\n","thread main create error!" );
+	if(pthread_create(&recv_thread,NULL,recvThread,NULL)!=0)
+		printf("%s\n","thread recv create error!" );
+
+	pthread_join(main_thread,NULL);
+	pthread_join(recv_thread,NULL);
+/*
 	while(fgets(sendline,MAXLINE,stdin)!=NULL){
 		send(sockfd,sendline,strlen(sendline),0);
 
@@ -42,4 +333,5 @@ int main(int argc,char **argv)
 		memset(recvline,0,MAXLINE);
 	}
 	exit(0);
+	*/
 }
--- server.c
+++ server.c
@@ -4,26 +4,307 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<string.h>
-
 #include<unistd.h>
+#include<pthread.h>
 
 #define MAXLINE 4096
 #define SERV_PORT 3000
 //maximum of require to be handled that can be stored
-#define LISTENQ 8
+#define LISTENQ 10
 
+//local database
 struct USER{
-	char name[8];
-	char key[8];
-	bool on_off;//1for on; 0 for off
-	int connfd;//for each user online
+	char name[10];
+	char key[10];
+	int tag;//1:avaliable  0:not free
+	
 };
 struct USER users[10];
 
-int main(int argc, char **argv)
+struct onlineUSER
 {
+	char name[10];
+	int on_off;//1for on; 0 for off
+	int connfd;//for each user online
+};
+struct onlineUSER onlineUsers[10];
+
+struct SENDTOCLI
+{
+	char ctrl1;
+	char ctrl2;
+	char from[10];
+	char dst[10];
+	char data[2048];
+
+};
+struct SENDTOCLI sorline;
+struct SENDTOCLI dstline;
+
+struct CLIENT_SEND
+{
+	char ctrl1;
+	char ctrl2;
+	char local_name[10];//your name
+	char local_key[10];
+	char dst_name[10];//whom to send to
+	char data[2048];
+};
+struct CLIENT_SEND recvl;
+
+char recvline[MAXLINE];
+char sendlineSor[MAXLINE];
+char sendlineDst[MAXLINE];
+int findOnlineStatus(char name[]);
+int findConnfd(char name[]);
+void loginVerify(char name[],char key[]);
+void registeVerify(char name[],char key[]);
+void unlogin(char name[]);
+int findDBFree();
+int findFree();
+void *dealing(void *j);
+
+int findOnlineStatus(char name[]){
+	for (int i = 0; i < 10; ++i)
+	{
+		if (strcmp(name,onlineUsers[i].name))
+		{
+			return onlineUsers[i].on_off;
+		}
+	}
+	printf("%s\n","can't find the name" );
+	return 0;
+}
+int findConnfd(char name[]){
+	for (int i = 0; i < 10; ++i)
+	{
+		if (strcmp(name,onlineUsers[i].name))
+		{
+			return onlineUsers[i].connfd;
+		}
+	}
+	printf("%s\n","can't find the name" );
+	return -1;
+
+}
+void loginVerify(char name[],char key[]){
+	for (int i = 0; i < 10; ++i)
+	{
+		if (strcmp(name,users[i].name))
+		{
+			if (strcmp(key,users[i].key))
+			{
+				sorline.ctrl1='3';
+				sorline.ctrl2='1';
+				printf("%s\n","login succeed" );
+				return ;
+			}else 
+			printf("%s\n","password incorrect" );
+		}
+		else ;
+	}
+
+	printf("%s\n","name not found" );
+	sorline.ctrl1='3';
+	sorline.ctrl2='2';
+	return ;
+}
+void registeVerify(char name[],char key[]){
+	for (int i = 0; i < 10; ++i)
+	{
+		if (strcmp(name,users[i].name))
+		{
+			sorline.ctrl1='3';
+			sorline.ctrl2='4';
+			printf("%s\n","name already exits" );
+			return ;
+		}
+	}
+
+	sorline.ctrl1='3';
+	sorline.ctrl2='3';
+	int i;
+	i=findDBFree();
+	strcpy(users[i].name,name);
+	strcpy(users[i].key,key);
+	users[i].tag=0;
+	printf("%s\n","registe succeed" );
+}
+void unlogin(char name[]){
+	for (int i = 0; i < 10; ++i)
+	{
+		if (strcmp(name,onlineUsers[i].name))
+		{
+			onlineUsers[i].on_off=0;//offline
+			sorline.ctrl1='5';
+			sorline.ctrl1='1';
+			return ;
+		}
+	}
+	sorline.ctrl1='5';
+	sorline.ctrl1='2';		
+	printf("%s\n","unlogin error" );
+}
+int findDBFree(){
+	for (int i = 0; i < 10; ++i)
+	{
+		if (users[i].tag==1)
+			return i;
+	}
+}
+int findFree(){
+	for (int i = 0; i < 10; ++i)
+	{
+		if (onlineUsers[i].on_off==0)
+			return i;
+	}
+}
+void *dealing(void *jj){
+/*--------------------getting data------------------------*/
+	int connfd;
+	int j=*(int*)jj;
+	connfd=onlineUsers[j].connfd;
+	recv(connfd,recvline,MAXLINE,0);/////////////////
+	recvl.ctrl1=recvline[0];
+	recvl.ctrl2=recvline[1];
+	for (int i = 0; i < 10; ++i)
+	{
+		recvl.local_name[i]=recvline[i+2];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		recvl.local_key[i]=recvline[i+12];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		recvl.dst_name[i]=recvline[i+22];
+	}
+	for (int i = 0; recvline[i+32]!='\0'; ++i)
+	{
+		recvl.data[i]=recvline[i+32];
+	}
+	strcmp(onlineUsers[j].name,recvl.local_name);
+/*--------------------getting data------------------------*/
+
+	if (recvl.ctrl1=='1')
+	{
+		if (recvl.ctrl2=='1')
+		{
+			loginVerify(recvl.local_name,recvl.local_key);
+		}
+		else if (recvl.ctrl2=='2')
+		{
+			registeVerify(recvl.local_name,recvl.local_key);
+		}
+	}
+	else if (recvl.ctrl1=='2')
+	{
+		if (recvl.ctrl2=='1')
+		{//dst client
+			dstline.ctrl1='4';
+			dstline.ctrl1='3';
+			strcpy(dstline.from,recvl.local_name);
+			strcpy(dstline.dst,recvl.dst_name);
+			strcpy(dstline.data,recvl.data);
+		//source client
+			sorline.ctrl1='4';
+			sorline.ctrl2='1';
+
+		}
+		else if (recvl.ctrl2=='2')
+		{
+			dstline.ctrl1='4';
+			dstline.ctrl1='3';
+			strcpy(dstline.from,recvl.local_name);
+			strcpy(dstline.dst,recvl.dst_name);//all
+			strcpy(dstline.data,recvl.data);
+
+			sorline.ctrl1='4';
+			sorline.ctrl2='1';
+		}
+		else 
+		{
+		sorline.ctrl1='4';
+		sorline.ctrl2='2';
+		}
+	}
+	else if (recvl.ctrl1=='3')
+	{
+		if (recvl.ctrl2=='1')
+		{
+			unlogin(recvl.local_name);
+		}
+	}
+	else 
+	{
+		sorline.ctrl1='0';
+		sorline.ctrl2='0';
+	}
+
+	sendlineSor[0]=sorline.ctrl1;
+	sendlineSor[1]=sorline.ctrl2;
+	for (int i = 0; i < 10; ++i)
+	{
+		sendlineSor[2+i]=sorline.from[i];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		sendlineSor[12+i]=sorline.dst[i];
+	}
+	for (int i = 0; sorline.data[i]!='\0'; ++i)
+	{
+		sendlineSor[22+i]=sorline.data[i];
+	}
+
+	sendlineDst[0]=dstline.ctrl1;
+	sendlineDst[1]=dstline.ctrl2;
+	for (int i = 0; i < 10; ++i)
+	{
+		sendlineDst[2+i]=dstline.from[i];
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		sendlineDst[12+i]=dstline.dst[i];
+	}
+	for (int i = 0; dstline.data[i]!='\0'; ++i)
+	{
+		sendlineDst[22+i]=dstline.data[i];
+	}
+
+	send(findConnfd(recvl.local_name),sendlineSor,MAXLINE,0);
+	if (recvl.ctrl1=='2'&&recvl.ctrl2=='2')//group send
+	{
+		for (int i = 0; i < 10; ++i)
+		{
+			if (onlineUsers[i].on_off)//if on
+			{
+				send(onlineUsers[i].connfd,sendlineDst,MAXLINE,0);
+			}
+		}
+	}
+	else send(findConnfd(recvl.dst_name),sendlineDst,MAXLINE,0);
+
+}
+
+int main()
+{
+/*-------------database initial--------------------*/
+	strcpy(users[0].name,"about");
+	strcpy(users[0].key,"112233");
+	strcpy(users[1].name,"blank");
+	strcpy(users[1].key,"112233");
+	strcpy(users[2].name,"test");
+	strcpy(users[2].key,"112233");
+	users[0].tag=0;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		onlineUsers[i].on_off=false;
+	}
+
+/*-------------database initial--------------------*/
 	int listenfd,connfd,n;
-	pid_t childpid;
+	//pid_t childpid;
 	socklen_t clilen;
 	char buf[MAXLINE];
 	struct sockaddr_in cliaddr,servaddr;
@@ -38,12 +319,20 @@ int main(int argc, char **argv)
 	bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
 
 	listen(listenfd,LISTENQ);
-
+	pthread_t thread[10];
 	printf("%s\n","Server running...waiting for connections." );
 
+	while(1){
+		clilen=sizeof(cliaddr);
+		int j=findFree();
+		onlineUsers[j].connfd=accept (listenfd,(struct sockaddr *) &cliaddr, &clilen);
+		pthread_create(&thread[j],NULL,dealing,(void *)&j);
+
+	}
+
+/*
 	for(;;){
 		clilen=sizeof(cliaddr);
-
 		connfd=accept (listenfd,(struct sockaddr *) &cliaddr, &clilen);
 		printf("%s\n","Received request..." );
 		/////
@@ -66,8 +355,9 @@ int main(int argc, char **argv)
 			printf("%s\n","Read error" );
 		exit(0);
 	}
+	
 	close(connfd);
-}
+}*/
 
 
 }
